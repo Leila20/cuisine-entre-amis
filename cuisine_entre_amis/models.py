6c658a234f3f91e4a71eba91c85faeb19be822cc
Leila <leila@levit.be>
2015-12-28 11:35:11 +0100

    :construction: WIP

--- cuisine_entre_amis/models.py
+++ cuisine_entre_amis/models.py
@@ -1,10 +1,25 @@
 from django.db import models
+from django.utils.translation import ugettext as _
+
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class Contact(models.Model):
-
-  name = models.CharField(max_length=100)
-  mail = models.EmailField(max_length=100)
-  phone = models.CharField(max_length=100)
-  message = models.TextField()
+  name = models.CharField(_('Name'), max_length=100)
+  mail = models.EmailField(_('E-mail'), max_length=100)
+  phone = models.CharField(_('Phone'), max_length=100)
+  message = models.TextField(_('Message'))
   created_at = models.DateTimeField(auto_now_add=True)
+
+  @property
+  def short(self):
+    if len(self.message) > 30:
+      return '{}...'.format(self.message[:27])
+    return self.message
+
+
+@receiver(post_save, sender=Contact)
+def contact_send_mail(sender, instance, **kwargs):
+  from .tasks import send_contact_mail
+  send_contact_mail.delay(instance.pk)
--- cuisine_entre_amis/views.py
+++ cuisine_entre_amis/views.py
@@ -1,18 +1,30 @@
 
 from django.views.generic.edit import FormView
+from django.views.generic.base import TemplateResponseMixin
 
 from django.core.urlresolvers import reverse_lazy
 
-from .models import Contact
 from .forms import ContactForm
 
 
-class ContactView(FormView):
+class AjaxResponseMixin(TemplateResponseMixin):
 
-  model = Contact
-  template_name = 'cuisine_entre_amis/contact.html'
+  html_template_name = None
+  ajax_template_name = None
+
+
+class AbstractContactView(object):
+
+  def get_context_data(self, **kwargs):
+    context = super(AbstractContactView, self).get_context_data(**kwargs)
+    return context
+
+
+class ContactView(AbstractContactView, FormView, AjaxResponseMixin):
+  html_template_name = 'jean_yves/contact.html'
+  ajax_template_name = "jean_yves/ajax-contact.html"
   form_class = ContactForm
-  success_url = reverse_lazy('/')
+  success_url = reverse_lazy('thanks')
 
   def form_valid(self, form):
     if form.should_save:
